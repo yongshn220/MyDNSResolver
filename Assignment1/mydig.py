3a1269a5ee3c31730c56763332e39fd8e2601f97
Yongjung Shin <yongshn220@gmail.com>
2023-02-16 23:48:16 -0500

    Fix | Error

--- Assignment1/mydig.py
+++ Assignment1/mydig.py
@@ -31,18 +31,21 @@ def send_error(code):
         print(f"Error: No corresponding IP to [{domain_input}].")
 
 
-def print_result(result):
-    if result is None:
+def print_result(answer, result):
+    if answer is None:
         send_error(2)
         exit(0)
 
     print("QUESTION SECTION:")
     print(f"{domain_input.ljust(37)}  {'IN'.ljust(5)} {'A'.ljust(6)}")
     print("")
-    print("ANSWER SECTION:")
+    if result:
+        print("ANSWER SECTION:")
+    else:
+        print("AUTHORITY SECTION:")
     for cname in cname_response:
         print(str(cname.name).ljust(30), str(cname.ttl).ljust(7), str_rdclass(cname[0].rdclass).ljust(5), str_rdtype(cname[0].rdtype).ljust(5), str(cname[0].target))
-    print(str(result.name).ljust(30), str(result.ttl).ljust(7), str_rdclass(result[0].rdclass).ljust(5), str_rdtype(result[0].rdtype).ljust(5), result[0].address)
+    print(str(answer.name).ljust(30), str(answer.ttl).ljust(7), str_rdclass(answer[0].rdclass).ljust(5), str_rdtype(answer[0].rdtype).ljust(5), answer[0].address)
     print("")
     print(f"Query time: {math.ceil((time.time() - start_time) * 1000)} msec")
     print(f"When: {datetime.datetime.now()}")
@@ -61,17 +64,17 @@ def main():
     domain_input = domain
     dname = dns.name.from_text(domain)
 
-    result = rec_roots(dname)
-    print_result(result)
+    answer, result = rec_roots(dname)
+    print_result(answer, result)
 
 
 # search from the root name server
 def rec_roots(dname):
     for rip in root_ns:
-        result = rec(rip, dname)
-        if result:
-            return result
-    return None
+        answer, result = rec(rip, dname)
+        if answer:
+            return answer, result
+    return None, False
 
 
 # recursively search following domain with ip.
@@ -79,14 +82,13 @@ def rec(ip, dname):
     query = dns.message.make_query(dname, dns.rdatatype.A)
     try:
         response = dns.query.udp(query, ip)
-        # response = dns.query.tcp(query, ip)
     except Exception:
-        return None
+        return None, False
 
     if len(response.answer) > 0:
         for ans in response.answer:
             if ans.rdtype == dns.rdatatype.A:
-                return ans
+                return ans, True
             if ans.rdtype == dns.rdatatype.CNAME:
                 cname_response.append(ans)
                 return rec_roots(ans[0].target)  # recursive from root
@@ -95,17 +97,21 @@ def rec(ip, dname):
             if rrset.rdtype != dns.rdatatype.A:
                 continue
             for rr in rrset:
-                result = rec(rr.address, dname)  # recursive
-                if result:
-                    return result
+                answer, result = rec(rr.address, dname)  # recursive
+                if answer:
+                    return answer, result
+                else:
+                    return rrset, result
     if len(response.authority) > 0:
         for rrset in response.authority[0]:
             if rrset.rdtype == dns.rdatatype.NS:
-                rrset = rec_roots(rrset.target)  # recursive root
-                result = rec(rrset[0].address, dname)  # recursive
-                if result:
-                    return result
-    return None
+                arrset, result = rec_roots(rrset.target)  # recursive root
+                answer, result = rec(arrset[0].address, dname)  # recursive
+                if answer:
+                    return answer, result
+                else:
+                    return rrset, result
+    return None, False
 
 
 # result
@@ -114,6 +120,5 @@ cname_response = []
 
 # init
 root_ns = ["198.41.0.4", "199.9.14.201", "192.33.4.12", "199.7.91.13", "192.203.230.10", "192.5.5.241", "192.112.36.4", "198.97.190.53", "192.36.148.17", "192.58.128.30", "193.0.14.129", "199.7.83.42"]
-root_ns = ["198.41.0.4"]
 start_time = time.time()
 main()
